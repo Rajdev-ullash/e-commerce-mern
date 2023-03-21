66a82855008e6e3809b559b178df5f2181ba9b68
Rajdev-ullash <rajdevullash111@gmail.com>
2023-03-21 12:40:37 +0600

    topnavbar complete

--- client/src/components/Navbar/TopNav.jsx
+++ client/src/components/Navbar/TopNav.jsx
@@ -1,12 +1,58 @@
 import React, { Fragment } from "react";
-
+import { MdOutlineAccountCircle } from "react-icons/md";
+import { AiOutlineHeart } from "react-icons/ai";
+import { Dropdown } from "flowbite-react";
 const TopNav = () => {
   return (
     <Fragment>
-      <div className="w-full container mx-auto">
-        <div className="flex items-center justify-between">
-          <div>Need help? call us : (+92) 0123 456 789</div>
-          <div>With warranty & free shipping for above $78.00</div>
+      <div className="bg-slate-50 font-Roboto text-gray-500 tracking-wide font-medium text-base">
+        <div className="w-full container mx-auto sm:px-32 sm:py-4">
+          <div className="flex items-center justify-between">
+            <div>
+              Need help? call us :{" "}
+              <span className="hover hover:text-red-500">
+                (+92) 0123 456 789
+              </span>
+            </div>
+            <div>With warranty & free shipping for above $78.00</div>
+            <div className="flex items-center">
+              <div className="">
+                <Dropdown
+                  arrowIcon={false}
+                  inline={true}
+                  label={
+                    <div className="flex items-center">
+                      <MdOutlineAccountCircle className="text-2xl text-gray-500" />
+                      <span className="pl-2">Account</span>
+                    </div>
+                  }
+                >
+                  <Dropdown.Header>
+                    <span className="block text-sm">Bonnie Green</span>
+                    <span className="block truncate text-sm font-medium">
+                      name@flowbite.com
+                    </span>
+                  </Dropdown.Header>
+                  <Dropdown.Item>Dashboard</Dropdown.Item>
+                  <Dropdown.Item>Settings</Dropdown.Item>
+                  <Dropdown.Item>Earnings</Dropdown.Item>
+                  <Dropdown.Divider />
+                  <Dropdown.Item>Sign out</Dropdown.Item>
+                </Dropdown>
+              </div>
+              {/* right straight line */}
+              <div className="h-4 border-r-2 border-gray-400 mx-2"></div>
+
+              <div className="flex items-center">
+                {/* wishlist icon */}
+                <div className="pr-2">
+                  <AiOutlineHeart className="text-2xl text-gray-500" />
+                </div>
+
+                <div>Wishlist</div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </Fragment>
--- client/tailwind.config.cjs
+++ client/tailwind.config.cjs
@@ -6,7 +6,11 @@ module.exports = {
     "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        Roboto: ["Roboto", "sans-serif"],
+      },
+    },
   },
   plugins: [require("flowbite/plugin")],
 };
